8094a57831d4c5f5411e06f5d34c5027837cdc59
Сергей <sergeystarod@mail.ru>
2022-11-07 23:01:48 +0300

    Мой первый коммит

--- src/Human.java
+++ src/Human.java
@@ -2,14 +2,16 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String jobTitle;
 
-    Human(String name, String town, int yearOfBirth){
+    Human(String name, String town, int yearOfBirth, String jobTitle){
         this.name = name;
         this.town = town;
         this.yearOfBirth = yearOfBirth;
+        this.jobTitle = jobTitle;
     }
 
     void message(){
-        System.out.println("Привет! Меня зовут " + name + "." + " Я из города " + town + "." + " Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + "." + " Я из города " + town + "." + " Я родился в " + yearOfBirth + " году. Я работаю на должности "+ jobTitle + ". " + "Будем знакомы!");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maxim = new Human("Максим", "Минск", 1988);
+        Human maxim = new Human("Максим", "Минск", 1988, "бренд-менеджер");
         maxim.message();
-        Human anya = new Human("Аня", "Москва",1993);
+        Human anya = new Human("Аня", "Москва", 1993, "методист образовательных программ");
         anya.message();
-        Human katia = new Human("Катя", "Клининград", 1992);
+        Human katia = new Human("Катя", "Клининград", 1992, "продакт-менеджер");
         katia.message();
-        Human artyom = new Human("Артём", "Москва", 1995);
+        Human artyom = new Human("Артём", "Москва", 1995, "директор по развитию бизнеса");
         artyom.message();
     }
 }
